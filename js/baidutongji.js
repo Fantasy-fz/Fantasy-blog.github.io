d7fb8aa214f6bbb9416c83f58ee8651d95fa71c0
777nx <81453606+777nx@users.noreply.github.com>
2023-01-11 13:44:50 +0000

    安装百度统计代码 Updated By Github Actions

---
+++ js/baidutongji.js
@@ -0,0 +1,1 @@
+var _hmt=_hmt||[];!function(){var e=document.createElement("script");e.src="https://hm.baidu.com/hm.js?26be843812d94aa145589c44dc8f7b9c";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)}();
